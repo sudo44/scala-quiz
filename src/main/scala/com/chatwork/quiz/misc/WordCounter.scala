46377d9392d15c2364c6320c3a3408a4fdd5980b
Yuji Sudo <sudo44@gmail.com>
2015-05-17 19:02:40 +0900

    add word count

--- src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
+++ src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
@@ -11,6 +11,9 @@ object WordCounter {
    * @param words 文字列
    * @return 単語がキー、単語数がヴァリューのマップ
    */
-  def countWords(words: List[String]): Map[String, Int] = ???
+  def countWords(words: List[String]): Map[String, Int] =
+    words.map(_.split(' ')).flatMap(x => x).groupBy(identity).mapValues(_.length)
+//    words.map(_.split(" ")).flatMap(x => x).groupBy(identity).mapValues(_.length)
 
 }
+
